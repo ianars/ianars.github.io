a0efa857818d763071c362c33ea8865fc273e6eb
ianars <iana.rusu.it@gmail.com>
2017-08-26 02:46:42 +0200

    media queries for tablet, added animated skills bars, added nav for tablet and desktop, and animations in the section projects

--- js/main.js
+++ js/main.js
@@ -20,12 +20,18 @@ $(document).ready(function(){
 
 	$(window).scroll(function(){
 		var barra = $(window).scrollTop();
-		var posicion =  (barra * 0.10);
+		var position =  (barra * 0.10);
 
 		$('body').css({
-			'background-position': '0 -' + posicion + 'px'
+			'background-position': '0 -' + position + 'px'
 		});
 
 	});
 
+});
+
+jQuery('.skillbar').each(function(){
+	jQuery(this).find('.skillbar-bar').animate({
+		width:jQuery(this).attr('data-percent')
+	},4000);
 });
