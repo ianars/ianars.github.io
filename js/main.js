d96d3e6110be2fcc699664023fe4aafea103b422
ianars <iana.rusu.it@gmail.com>
2017-08-29 17:37:07 +0200

    changed social icons, modified desktop media queries, add arrow to return to top

--- js/main.js
+++ js/main.js
@@ -25,13 +25,25 @@ $(document).ready(function(){
 		$('body').css({
 			'background-position': '0 -' + position + 'px'
 		});
-
 	});
-
 });
 
 jQuery('.skillbar').each(function(){
 	jQuery(this).find('.skillbar-bar').animate({
 		width:jQuery(this).attr('data-percent')
 	},4000);
+});
+
+// ===== Scroll to Top ====
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
 });
